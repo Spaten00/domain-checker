57da38f986ddd7b5e2584ccc5da75e8ff05f4b01
Ludwig Schippers <se@aks-service.com>
2021-11-01 20:02:38 +0100

    Remove unused methods; Exclude ProvisionImport from test coverage

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -10,26 +10,6 @@ use Illuminate\Http\Request;
 class BillController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param StoreBillRequest $request
@@ -48,28 +28,6 @@ class BillController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Bill $bill
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Bill $bill)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Bill $bill
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Bill $bill)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
@@ -81,16 +39,5 @@ class BillController extends Controller
         $bill->bill_number = $request->newNumber;
         $bill->save();
         return response("Letzte Rechnungsnummer geändert. ");
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Bill $bill
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Bill $bill)
-    {
-        //
     }
 }
--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -11,26 +11,6 @@ use Illuminate\Http\Response;
 class ContractController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param StoreContractRequest $request
@@ -49,28 +29,6 @@ class ContractController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param Contract $contract
-     * @return Response
-     */
-    public function show(Contract $contract)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Contract $contract
-     * @return Response
-     */
-    public function edit(Contract $contract)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param Request $request
@@ -82,16 +40,5 @@ class ContractController extends Controller
         $contract->contract_number = $request->newNumber;
         $contract->save();
         return response("Vertragsnummer geändert. ");
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Contract $contract
-     * @return Response
-     */
-    public function destroy(Contract $contract)
-    {
-        //
     }
 }
